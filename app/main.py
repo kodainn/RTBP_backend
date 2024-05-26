6ec0754320ded95cde0d77b1c8e9714310673e2c
kodai <kodai030513@gmail.com>
2024-05-26 11:10:18 +0900

    CORSのミドルウェア追加

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.routers.auth import router as auth_router
 from app.routers.users import router as users_router
 from app.routers.dashboard import router as dashboard_router
@@ -8,6 +10,14 @@ from app.routers.studied_history_books import router as studied_history_books_ro
 from app.routers.studying_books import router as studying_books_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PATCH", "DELETE"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 app.include_router(users_router)
